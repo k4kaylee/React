89fb780d51a7944711f6182c4f00b53f079dd907
k4kaylee <roman.kulpeksha@mail.ru>
2021-10-28 21:18:33 +0300

    My Modal with overlay, Loader were  added

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {React, useState} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Users  from '../pages/Users'
 import Devices  from '../pages/Devices'
-
+import Loader from 'react-loader-spinner'
 
 const AppRoutes = () => {
+  const [loading, setLoading] = useState(true);
+  setTimeout(()=>{
+    setLoading(false)
+  },3000)
+  if(loading){
+    return <Loader color="#ee6e73" timeout={3000}/>
+  }
   return(
+
     <Switch>
       <Route path='/users' component={Users}/>
       <Route path='/devices' component={Devices}/>
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,103 +1,112 @@
 import React, { useState } from "react";
 import UserList from "../components/UserList";
+import MyModal from "../components/MyModal/MyModal"
 
 const Users = () => {
   const [showFormUser, setShowFormUser] = useState(false);
   const [users, setUsers] = useState([
     {
-      id:1,
-     name:'John',
-     phone: '7788'
+      id: 1,
+      name: 'John',
+      phone: '7788'
     },
     {
-      id:2,
-      name:'Thanos',
+      id: 2,
+      name: 'Thanos',
       phone: '50%'
-     },
-     {
-      id:3,
-      name:'Boris',
+    },
+    {
+      id: 3,
+      name: 'Boris',
       phone: '5467'
-     },
+    },
   ]);
   const [user, setUser] = useState(
     {
-    name:'',
-    phone:'',
+      name: '',
+      phone: '',
     });
 
   const onChange = (e) => {
-    if (e.target.id == "name"){
-      setUser({...user, name: e.target.value});
+    if (e.target.id == "name") {
+      setUser({ ...user, name: e.target.value });
     } else {
-      setUser({...user, phone: e.target.value});
+      setUser({ ...user, phone: e.target.value });
     }
   }
 
-  const addUser = () =>{
-    const id = Math.random()*1
-    setUser({...user, id:id})
-    setUsers([...users,user]);
-    setUser( {
-      name:'',
-      phone:'',
-      });
+  const addUser = () => {
+    const id = Math.random() * 1
+    setUser({ ...user, id: id })
+    setUsers([...users, user]);
+    setUser({
+      name: '',
+      phone: '',
+    });
   }
 
-  const removeUser = (id)=>{
+  const removeUser = (id) => {
     const confirm = window.confirm("Реально удалить?")
-      if (confirm == true) setUsers(users.filter((user)=>user.id !== id)) //для проверки на удаление
+    if (confirm == true) setUsers(users.filter((user) => user.id !== id)) //для проверки на удаление
   };
-  const clear=()=>{
-    setUser({name:'', phone:''})
+  const clear = () => {
+    setUser({ name: '', phone: '' })
   }
   console.log(user);
+  const [showModal, setshowModal] = useState(false)
+
+
+
 
   return (
     <div className="App">
+
       <div className="container">
+        <MyModal visible={showModal} setVisible={setshowModal}>
+          {
+                 <>
+                 <div className="input-field col s6">
+                   <i className="material-icons prefix">account_circle</i>
+                   <input
+                     id="name"
+                     type="text"
+                     className="validate"
+                     value={user.name}
+                     placeholder="Enter Name"
+                     onChange={onChange}
+                   />
+                   <a className="waves-effect waves-light btn m-1"
+                     onClick={() => addUser()}>Add</a>
+                 </div>
+                 <div className="input-field col s6">
+                   <i className="material-icons prefix">phone</i>
+                   <input
+                     id="phone"
+                     type="tel"
+                     value={user.phone}
+                     className="validate"
+                     placeholder="Enter Phone"
+                     onChange={onChange}
+                   />
+                   <a className="waves-effect waves-light right btn m-1"
+                     onClick={() => clear()}>Clear</a>
+                 </div>
+               </>
+          }
+        </MyModal>
         <div className="row m-1">
           <div className="col s4">
             <a className="waves-effect waves-light btn"
-              onClick={()=>setShowFormUser(!showFormUser)}
-              >
-                  Add user
+              onClick={() => setshowModal(true)}
+            >
+              Add user
             </a>
           </div>
           <div className="col s8">
-           {showFormUser &&
-           <>
-            <div className="input-field col s6">
-              <i className="material-icons prefix">account_circle</i>
-              <input
-              id="name"
-              type="text"
-              class="validate"
-              value={user.name}
-              placeholder="Enter Name"
-              onChange={onChange}
-              />
-              <a className="waves-effect waves-light btn m-1"
-                onClick={()=>addUser()}>Add</a>
-            </div>
-            <div className="input-field col s6">
-              <i className="material-icons prefix">phone</i>
-              <input
-              id="phone"
-              type="tel"
-              value={user.phone}
-              className="validate"
-              placeholder="Enter Phone"
-              onChange={onChange}
-              />
-              <a className="waves-effect waves-light right btn m-1"
-              onClick={()=>clear()}>Cancel</a>
-            </div>
-           </> }
+
           </div>
         </div>
-            <UserList search deleteUser = {removeUser}>{users}</UserList>
-
+        <UserList search deleteUser={removeUser}>{users}</UserList>
       </div>
     </div>
   );
