1c89b75e6ca2552355708b1ff25d9c636a04364d
k4kaylee <roman.kulpeksha@mail.ru>
2021-11-08 11:43:28 +0300

    Posts were added

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom'
 import Users  from '../pages/Users'
 import Devices  from '../pages/Devices'
 import Home from '../pages/Home'
+import Posts from '../pages/Posts'
 import Loader from 'react-loader-spinner'
 
 const AppRoutes = () => {
@@ -18,6 +19,7 @@ const AppRoutes = () => {
     <Switch>
       <Route path='/users' component={Users}/>
       <Route path='/devices' component={Devices}/>
+      <Route path='/posts' component={Posts}/>
       <Route path='/' component={Home}/>
     </Switch>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,6 +9,7 @@ const NavBar = () => {
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/users'>Users</Link></li>
+        <li><Link to='/posts'>Posts</Link></li>
         <li><Link to='/devices'>Devices</Link></li>
         <li><a href="#">JavaScript</a></li>
       </ul>
